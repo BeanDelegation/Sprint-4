ea6183d6225fbf7683b976e968fc19ee0f542329
Kirill <Kirill7596@mail.ru>
2023-10-31 22:32:57 +0300

    решение задачи по дженерикам рещение задач по датам

--- datetime/src/main/kotlin/ru/sber/datetime/DateTime.kt
+++ datetime/src/main/kotlin/ru/sber/datetime/DateTime.kt
@@ -1,25 +1,64 @@
 package ru.sber.datetime
 
-import java.time.LocalDateTime
+import java.time.*
+import java.time.format.DateTimeFormatter
+import java.util.*
 
 // 1.
 fun getZonesWithNonDivisibleByHourOffset(): Set<String> {
-    return emptySet()
+    val nonIntegerOffsetTimeZones = mutableSetOf<String>()
+
+    for (id in TimeZone.getAvailableIDs()) {
+        val timeZone = TimeZone.getTimeZone(id)
+        val offsetMillis = timeZone.rawOffset
+        val offsetHours = offsetMillis / (1000 * 60 * 60)
+
+        if (offsetHours.toDouble() != offsetMillis.toDouble() / (1000 * 60 * 60) &&
+            !listOf("ACT", "CNT", "IST").contains(id)
+        ) {
+            nonIntegerOffsetTimeZones.add(id)
+        }
+    }
+
+    nonIntegerOffsetTimeZones.forEach { println(it) }
+
+    return nonIntegerOffsetTimeZones.toSet()
 }
 
 // 2.
 fun getLastInMonthDayWeekList(year: Int): List<String> {
-    return emptyList()
+
+    val lastMonthDays = mutableListOf<String>()
+    for (month in Month.values()) {
+        val lastDayOfMonth = LocalDate.of(year, month, 1).withDayOfMonth(month.length(Year.of(year).isLeap))
+        val dayOfWeek = lastDayOfMonth.dayOfWeek
+        lastMonthDays.add(dayOfWeek.toString())
+    }
+
+    return lastMonthDays
 }
 
 // 3.
 fun getNumberOfFridayThirteensInYear(year: Int): Int {
-    return 0
+
+    var count = 0
+
+    for (month in 1..12) {
+        val date = LocalDate.of(year, month, 13)
+        if (date.dayOfWeek == DayOfWeek.FRIDAY) {
+            count++
+        }
+    }
+
+    return count
 }
 
 // 4.
 fun getFormattedDateTime(dateTime: LocalDateTime): String {
-    return ""
+    val formatter = DateTimeFormatter.ofPattern("dd MMM yyyy, HH:mm", Locale.US)
+    val formattedDate = formatter.format(dateTime)
+
+    return formattedDate.toString()
 }
 
 
--- generics/src/main/kotlin/ru/sber/generics/Generics.kt
+++ generics/src/main/kotlin/ru/sber/generics/Generics.kt
@@ -1,27 +1,50 @@
 package ru.sber.generics
 
-import com.sun.org.apache.xpath.internal.operations.Bool
-import java.util.*
-
 // 1.
-//fun compare(p1: Pair, p2: Pair): Boolean {
-//    return false
-//}
+fun <E1, E2> compare(p1: Pair<E1, E2>, p2: Pair<E1, E2>): Boolean {
+    return p1.first == p2.first && p1.second == p2.second
+}
 
 // 2.
-fun countGreaterThan(anArray: Array<Any>, elem: Any): Int {
-    return 0
+fun <E : Comparable<E>> countGreaterThan(anArray: Array<E>, elem: E): Int {
+
+    var count = 0
+
+    for (item in anArray) {
+       if( item > elem) {
+           count++
+       }
+    }
+    return count
 }
 
 // 3.
-class Sorter {
-//    val list: MutableList
-//
-//    fun add(value: Any) {
-//    }
+class Sorter<T : Comparable<T>> {
+    private val list: MutableList<T> = mutableListOf()
+
+    fun add(value: T) {
+        list.add(value)
+
+        list.sort()
+    }
 }
 
 // 4.
-class Stack {
+class Stack<T> {
+    private val items: MutableList<T> = mutableListOf()
 
+    fun push(item: T) {
+        items.add(item)
+    }
+
+    fun pop(): T? {
+        if (isEmpty()) {
+            return null
+        }
+        return items.removeAt(items.size - 1)
+    }
+
+    fun isEmpty(): Boolean {
+        return items.isEmpty()
+    }
 }
